fda7bea4bd26a119f0e2f5df89a09596a93b56f9
maria macedo <69858181+Mariamacedo-prog@users.noreply.github.com>
2023-01-23 11:32:29 -0300

    Update Tmdb.js

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -7,7 +7,7 @@ const API_BASE = "https://api.themoviedb.org/3";
  */
 
 const basicFetch = async (endpoint) => {
-  const req = await fetch(`${API_BASE}${endpoint}`,{referrerPolicy: `${API_KEY}`});
+  const req = await fetch(`${API_BASE}${endpoint}`,{referrerPolicy: `${API_BASE}`});
   const json = req.json();
   return json;
 };
